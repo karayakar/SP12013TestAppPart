43d5a31c406f25d3d92e144688a64ab59401e976
colcook83 <colin.cook83@outlook.com>
2016-01-14 21:49:01 +0800

    First Commit

---
+++ AppPartTest/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="fef5ded3-0783-410a-bc19-9260f68718a5" description="SharePoint App Feature" featureId="fef5ded3-0783-410a-bc19-9260f68718a5" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="AppPartTest Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="a98de944-cfd5-4651-9321-76740a3b8778" />
+    <projectItemReference itemId="0239656d-bc32-4ec9-811a-f186655d5e7d" />
+    <projectItemReference itemId="e1ae3597-3269-4161-bdef-adc8d9149e93" />
+    <projectItemReference itemId="2a74c57f-1d89-4a58-a8e5-26a829de1e6d" />
+  </projectItems>
+</feature>
---
+++ AppPartTest/Scripts/App.js
@@ -0,0 +1,80 @@
+﻿'use strict';
+
+var context = SP.ClientContext.get_current();
+var user = context.get_web().get_currentUser();
+// Alternative method to get Url Parameter
+// var hostUrl = GetUrlKeyValue('SPHostUrl');
+var hostUrl = decodeURIComponent(getQueryStringParameter("SPHostUrl"));
+var appWebUrl = decodeURIComponent(getQueryStringParameter("SPAppWebUrl"));
+var scriptbase = hostUrl + "/_layouts/15/";
+
+// This code runs when the DOM is ready and creates a context object which is needed to use the SharePoint object model
+$(document).ready(function () {
+    getUserName();
+    // Load the js files and continue to the successHandler
+    $.getScript(scriptbase + "SP.RequestExecutor.js", getAllUserProfiles);
+});
+
+// This function prepares, loads, and then executes a SharePoint query to get the current users information
+function getUserName() {
+    context.load(user);
+    context.executeQueryAsync(onGetUserNameSuccess, onGetUserNameFail);
+}
+
+// This function is executed if the above call is successful
+// It replaces the contents of the 'message' element with the user name
+function onGetUserNameSuccess() {
+    $('#message').text('Hello ' + user.get_title());
+}
+
+// This function is executed if the above call fails
+function onGetUserNameFail(sender, args) {
+    alert('Failed to get user name. Error:' + args.get_message());
+}
+
+function getAllUserProfiles() {
+    // executor: The RequestExecutor object
+    // Initialize the RequestExecutor with the add-in web URL.
+    var executor = new SP.RequestExecutor(appWebUrl);
+    var fullUrl = appWebUrl + "/_api/SP.AppContextSite(@target)/web/lists?$orderby=Title&$select=Title&@target='" + encodeURIComponent(hostUrl) + "'";
+
+    $('#pageContext').text(fullUrl);
+
+    // Issue the call against the add-in web.
+    executor.executeAsync(
+        {
+            url: fullUrl,
+            type: "GET",
+            headers: {
+                "accept": "application/json; odata=verbose",
+                "content-type": "application/json; odata=verbose"
+            },
+            success: onGetAllUserProfilesSucceeded,
+            error: onGetAllUserProfilesFailed
+        }
+    );
+}
+
+function onGetAllUserProfilesSucceeded(data) {
+    $('#data').empty()
+    var jsonObject = JSON.parse(data.body);
+    var results = jsonObject.d.results;
+    for (var i = 0; i < results.length; i++) {
+        $("<p>" + i + " " + results[i].Title + "</p>").appendTo('#data')
+    }
+
+}
+
+function onGetAllUserProfilesFailed(data) {
+    $('#data').text('Failed to load data');
+}
+
+function getQueryStringParameter(urlParameterKey) {
+    var params = document.URL.split('?')[1].split('&');
+    var strParams = '';
+    for (var i = 0; i < params.length; i = i + 1) {
+        var singleParam = params[i].split('=');
+        if (singleParam[0] == urlParameterKey)
+            return decodeURIComponent(singleParam[1]);
+    }
+}
